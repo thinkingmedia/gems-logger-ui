dce782576efccf13c251a375d3e0b2208205771b
thinkingmedia <mathew@thinkingmedia.ca>
2016-01-17 15:40:19 -0500

    first commit

---
+++ LoggerUI/LoggerControl.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace LoggerUI
+{
+    partial class LoggerControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param _name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnMark = new System.Windows.Forms.Button();
+            this.checkShowDetails = new System.Windows.Forms.CheckBox();
+            this.checkShowFinest = new System.Windows.Forms.CheckBox();
+            this.chkShowDebug = new System.Windows.Forms.CheckBox();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbLog
+            // 
+            this.lbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.HorizontalScrollbar = true;
+            this.lbLog.ItemHeight = 14;
+            this.lbLog.Location = new System.Drawing.Point(0, 29);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.ScrollAlwaysVisible = true;
+            this.lbLog.Size = new System.Drawing.Size(568, 551);
+            this.lbLog.TabIndex = 8;
+            this.lbLog.DoubleClick += new System.EventHandler(this.onDoubleClick);
+            this.lbLog.MouseDown += new System.Windows.Forms.MouseEventHandler(this.onMouseDown);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnMark);
+            this.panel4.Controls.Add(this.checkShowDetails);
+            this.panel4.Controls.Add(this.checkShowFinest);
+            this.panel4.Controls.Add(this.chkShowDebug);
+            this.panel4.Controls.Add(this.btnClearLog);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(568, 29);
+            this.panel4.TabIndex = 9;
+            // 
+            // btnMark
+            // 
+            this.btnMark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMark.Location = new System.Drawing.Point(422, 3);
+            this.btnMark.Name = "btnMark";
+            this.btnMark.Size = new System.Drawing.Size(59, 23);
+            this.btnMark.TabIndex = 4;
+            this.btnMark.Text = "Mark";
+            this.btnMark.UseVisualStyleBackColor = true;
+            this.btnMark.Click += new System.EventHandler(this.onBtnMark);
+            // 
+            // checkShowDetails
+            // 
+            this.checkShowDetails.AutoSize = true;
+            this.checkShowDetails.Checked = true;
+            this.checkShowDetails.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkShowDetails.Location = new System.Drawing.Point(196, 6);
+            this.checkShowDetails.Name = "checkShowDetails";
+            this.checkShowDetails.Size = new System.Drawing.Size(88, 17);
+            this.checkShowDetails.TabIndex = 3;
+            this.checkShowDetails.Text = "Show Details";
+            this.checkShowDetails.UseVisualStyleBackColor = true;
+            this.checkShowDetails.CheckedChanged += new System.EventHandler(this.onShowDetails);
+            // 
+            // checkShowFinest
+            // 
+            this.checkShowFinest.AutoSize = true;
+            this.checkShowFinest.Location = new System.Drawing.Point(106, 6);
+            this.checkShowFinest.Name = "checkShowFinest";
+            this.checkShowFinest.Size = new System.Drawing.Size(84, 17);
+            this.checkShowFinest.TabIndex = 2;
+            this.checkShowFinest.Text = "Show Finest";
+            this.checkShowFinest.UseVisualStyleBackColor = true;
+            this.checkShowFinest.CheckedChanged += new System.EventHandler(this.onShowFinest);
+            // 
+            // chkShowDebug
+            // 
+            this.chkShowDebug.AutoSize = true;
+            this.chkShowDebug.Location = new System.Drawing.Point(12, 7);
+            this.chkShowDebug.Name = "chkShowDebug";
+            this.chkShowDebug.Size = new System.Drawing.Size(88, 17);
+            this.chkShowDebug.TabIndex = 1;
+            this.chkShowDebug.Text = "Show Debug";
+            this.chkShowDebug.UseVisualStyleBackColor = true;
+            this.chkShowDebug.CheckedChanged += new System.EventHandler(this.onShowDebug);
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearLog.Location = new System.Drawing.Point(487, 3);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 0;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.onBtnClear);
+            // 
+            // LoggerControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.panel4);
+            this.Name = "LoggerControl";
+            this.Size = new System.Drawing.Size(568, 580);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.CheckBox chkShowDebug;
+        private System.Windows.Forms.CheckBox checkShowFinest;
+        private System.Windows.Forms.CheckBox checkShowDetails;
+        private System.Windows.Forms.Button btnMark;
+    }
+}
---
+++ LoggerUI/LoggerControl.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Logging;
+using Logging.Formatters;
+using Logging.Writers;
+
+namespace LoggerUI
+{
+    /// <summary>
+    /// A control the prints the contents of the Log file.
+    /// </summary>
+    public partial class LoggerControl : UserControl
+    {
+        /// <summary>
+        /// Logging
+        /// </summary>
+        private static readonly Logger _logger = Logger.Create(typeof(LoggerControl));
+
+        /// <summary>
+        /// Used to find marker lines in the log stream.
+        /// </summary>
+        private const string _MARKER = "--MARKER--";
+
+        /// <summary>
+        /// The Log writer.
+        /// </summary>
+        private readonly ThreadWriter _threadWriter;
+
+        /// <summary>
+        /// Handles writing to multiple writers.
+        /// </summary>
+        private readonly MultiWriter _writer;
+
+        /// <summary>
+        /// A counter for placing markers in the Log.
+        /// </summary>
+        private static int _markCounter;
+
+        /// <summary>
+        /// Sets what thread this logger filters.
+        /// </summary>
+        public int threadID
+        {
+            get { return _threadWriter.ThreadID; }
+            set { _threadWriter.ThreadID = value; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LoggerControl()
+        {
+            InitializeComponent();
+
+            FormatWriter format = new FormatWriter(new ListBoxWriter(lbLog), new DetailFormat());
+
+            _threadWriter = new ThreadWriter(format);
+            _writer = new MultiWriter
+                      {
+                          new ContainsWriter(_threadWriter, _MARKER, true), 
+                          new ContainsWriter(format, _MARKER, false)
+                      };
+        }
+
+        /// <summary>
+        /// Tells the component the application has started.
+        /// </summary>
+        public void onStart()
+        {
+            Logger.Add(_writer);
+        }
+
+        /// <summary>
+        /// Tells the component the application is shutting down.
+        /// </summary>
+        public void onStop()
+        {
+            Logger.Remove(_writer);
+        }
+
+        /// <summary>
+        /// Changes the status of the Log debug flag.
+        /// </summary>
+        private void onShowDebug(object pSender, EventArgs pEventArgs)
+        {
+            Logger.LogDebug = chkShowDebug.Checked;
+            _logger.Fine("{0} Log debug mode: {1}", _MARKER, Logger.LogDebug.ToString());
+        }
+
+        /// <summary>
+        /// Changes the status of the Log Finest flag.
+        /// </summary>
+        private void onShowFinest(object pSender, EventArgs pEventArgs)
+        {
+            Logger.LogFinest = checkShowFinest.Checked;
+            _logger.Fine("{0} Log Finest mode: {1}", _MARKER, Logger.LogFinest.ToString());
+        }
+
+        /// <summary>
+        /// Changes the status of the Log details flag.
+        /// </summary>
+        private void onShowDetails(object pSender, EventArgs pEventArgs)
+        {
+            Logger.LogDetails = checkShowDetails.Checked;
+            _logger.Fine("{0} Log details mode: {1}", _MARKER, Logger.LogDetails.ToString());
+        }
+
+        /// <summary>
+        /// Clears the contents of the logger window.
+        /// </summary>
+        private void onBtnClear(object pSender, EventArgs pEventArgs)
+        {
+            lbLog.Items.Clear();
+        }
+
+        /// <summary>
+        /// Places a marker in the Log file.
+        /// </summary>
+        private void onBtnMark(object pSender, EventArgs pEventArgs)
+        {
+            _logger.Fine("{0} ***********************************************", _MARKER);
+            _logger.Fine("{0} - {1} -", _MARKER, _markCounter++);
+            _logger.Fine("{0} ***********************************************", _MARKER);
+        }
+
+        /// <summary>
+        /// Right click to clear selection.
+        /// </summary>
+        private void onMouseDown(object pSender, MouseEventArgs pEventArgs)
+        {
+            if (pEventArgs.Button == MouseButtons.Right)
+            {
+                lbLog.SelectedIndex = -1;
+            }
+        }
+
+        /// <summary>
+        /// Open the current item in the default browser if it contains a URL.
+        /// </summary>
+        private void onDoubleClick(object pSender, EventArgs pEventArgs)
+        {
+            string str = lbLog.SelectedItem.ToString();
+
+            Match match = Regex.Match(str, @"(https?://.*)");
+            if (!match.Success)
+            {
+                return;
+            }
+
+            string url = match.Groups[0].Value.Trim();
+            if (!string.IsNullOrEmpty(url))
+            {
+                Process.Start(url);
+            }
+        }
+    }
+}
